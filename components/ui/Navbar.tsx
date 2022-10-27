92bbd1406f9a5ec7038e1c033583158870f3728b
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-26 18:51:43 -0600

    user is now able to open/close sideDrawer

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
+
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import { UIContext } from '../../context/ui/UIContext';
 
 export const Navbar = () => {
+  const { openSideDrawer } = useContext(UIContext);
   return (
     <AppBar position="sticky">
       <Toolbar>
-        <IconButton>
+        <IconButton onClick={openSideDrawer}>
           <MenuOutlinedIcon />
         </IconButton>
         <Typography variant="h6">Dynamic Tasks</Typography>
