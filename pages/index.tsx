b7c1a9ca50a630af7afa464963b98658ed9bc091
ing-arriola <ingeniero.arriola@gmail.com>
2022-10-26 18:51:03 -0600

    Added design for indexPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import { NextPage } from 'next';
+import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
-  return <h1>Hello There</h1>;
+  return (
+    <Layout title="Home">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Pendings" />
+            <CardContent>{/*  */}</CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
 };
 
 export default HomePage;
