9dab47c8e0ff52566ba99715e5f850e58e1d8410
ing-arriola <ingeniero.arriola@gmail.com>
2022-11-02 07:00:08 -0600

    completed drag&drop

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,24 +1,44 @@
+import { DragEvent } from 'react';
 import { List, Paper } from '@mui/material';
 import { EntryStatus } from '../../interfaces';
 import { EntryCard } from './';
 import { useContext, useMemo } from 'react';
 import { EntriesContext } from '../../context/entries/EntriesContext';
-
+import { UIContext } from '../../context/ui/UIContext';
+import styles from './EntryList.module.css';
 interface EntryListProps {
   status: EntryStatus;
 }
 
 export const EntryList = ({ status }: EntryListProps) => {
-  const { entries } = useContext(EntriesContext);
-
+  const { entries, updateEntry } = useContext(EntriesContext);
+  const { isDragging, onDragEnded } = useContext(UIContext);
   const entriedByStatus = useMemo(
     () => entries.filter((entry) => entry.status === status),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [entries]
   );
 
+  const allowDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    console.log(id);
+  };
+
+  const onDropEntry = (event: DragEvent<HTMLDivElement>) => {
+    const id = event.dataTransfer.getData('text');
+    const entry = entries.find((e) => e._id === id)!;
+    entry.status = status;
+    updateEntry(entry);
+    onDragEnded();
+  };
+
   return (
-    <div>
+    <div
+      className={isDragging ? styles.dragging : ''}
+      onDrop={onDropEntry}
+      onDragOver={allowDrop}
+    >
       <Paper
         sx={{
           height: 'calc(100vh - 250px)',
@@ -27,7 +47,7 @@ export const EntryList = ({ status }: EntryListProps) => {
           padding: '1px 5px',
         }}
       >
-        <List sx={{ opacity: 1 }}>
+        <List sx={{ opacity: isDragging ? 0 : 1 }}>
           {entriedByStatus.map((entry) => (
             <EntryCard key={entry._id} entry={entry} />
           ))}
